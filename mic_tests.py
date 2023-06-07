bf268d6ba2819f36749a29e56b14f16c17017b2b
fifthaccess <benedikt.erkner-sach@bulme.at>
2023-06-07 16:19:23 +0200

    index

--- mic_tests.py
+++ mic_tests.py
@@ -1,9 +1,14 @@
 import speech_recognition as sr
+import pyaudio 
 
-with sr.Microphone() as source:
+
+
+
+with sr.Microphone(device_index=2) as source:
     print("listening: ")
     listner = sr.Recognizer()
     voice = listner.listen(source)
     command = listner.recognize_google(voice, language= 'de-at')#, show_all=True
     print(command)
-    
+
+
