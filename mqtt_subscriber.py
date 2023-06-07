d82bd715b3bb4e5f33202cac74b57ef711f1d34b
fifthaccess <benedikt.erkner-sach@bulme.at>
2023-06-07 15:54:56 +0200

    rename

---
+++ mqtt_subscriber.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+
+# The callback function of connection
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    client.subscribe("testTopic")
+
+# The callback function for received message
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("172.16.119.10", 1883, 60)
+client.loop_forever()
