817511b9019c99fe3b9ecf0f68cae85926db661d
fifthaccess <benedikt.erkner-sach@bulme.at>
2023-06-14 14:32:30 +0200

    adding esp savefile

--- esp.cpp
+++ esp.cpp
@@ -4,70 +4,74 @@
 
 const char SSID = "DietPi-HotSpot";
 const charPWD = "dietpihotspot";
+const char* mqttServer = "172.16.119.10"; 
+const int mqttPort = 1883; 
+const int ledPin = 2;
+const int signalPin = 4;
 
-// MQTT client
 WiFiClient wifiClient;
-PubSubClient mqttClient(wifiClient); 
+PubSubClient mqttClient(wifiClient);
 
-
-void callBack(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Callback - ");
-  Serial.print("Message:");
-  for (int i = 0; i < length; i++) {
+void callback(char* topic, byte* payload, unsigned int length) {
+  // Nachricht empfangen
+  Serial.print("Nachricht empfangen: ");
+  /*for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
-}
-char *mqttServer = "192.168.42.1";
-int mqttPort = 1883;
+  */
+  Serial.println();
 
-void setupMQTT() {
-  mqttClient.setServer(mqttServer, mqttPort);
-  // set the callback function
-  mqttClient.setCallback(callBack);
+  //LED einschalten
+  if ((char)payload[0] == '1'){
+    Serial.println("1 recieved");
+    digitalWrite(ledPin, HIGH);
+  }
+  // LED auschalten
+  else if((char)payload[0] == '0'){
+    Serial.println("0 recieved");
+    digitalWrite(ledPin, LOW);
+  } 
 }
 
 void connectToWiFi() {
-  Serial.print("Connectiog to wifi ");
- 
-  WiFi.begin(SSID, PWD);
-  Serial.println(SSID);
+  Serial.print("Verbinde mit WiFi...");
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
     delay(500);
+    Serial.print(".");
   }
-  Serial.print("Connected.");
-
+  Serial.println();
+  Serial.println("WiFi verbunden");
 }
 
-void reconnect() {
-  Serial.println("Connecting to MQTT Broker...");
+void connectToMQTT() {
+  Serial.print("Verbinde mit MQTT-Broker...");
   while (!mqttClient.connected()) {
-      Serial.println("Reconnecting to MQTT Broker..");
-      String clientId = "ESP32Client-";
-      clientId += String(random(0xffff), HEX);
-
-      if (mqttClient.connect(clientId.c_str())) {
-        Serial.println("Connected.");
-        // subscribe to topic
-        mqttClient.subscribe("testTopic");
-      }
-
+    if (mqttClient.connect("ESP32Client")) {
+      Serial.println("verbunden");
+      mqttClient.subscribe("testTopic");
+    } else {
+      Serial.print("Fehlgeschlagen, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" Versuche es erneut in 5 Sekunden...");
+      delay(5000);
+    }
   }
 }
-
-
-
 
 void setup() {
   Serial.begin(9600);
-  connectToWiFi();
-  setupMQTT();
+  pinMode(ledPin, OUTPUT);
+  pinMode(signalPin, INPUT);
 
+  connectToWiFi();
+  mqttClient.setServer(mqttServer, mqttPort);
+  mqttClient.setCallback(callback);
 }
 
 void loop() {
-  if (!mqttClient.connected())
-  reconnect();
+  if (!mqttClient.connected()) {
+    connectToMQTT();
+  }
   mqttClient.loop();
-
 }
