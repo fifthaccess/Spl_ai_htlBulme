493f1b586a4117c92278e1590b200255599b47d4
fifthaccess <benedikt.erkner-sach@bulme.at>
2023-06-26 08:07:09 +0200

    testing voice reco with opernAI

--- openai_tranlate.py
+++ openai_tranlate.py
@@ -1,7 +1,18 @@
 import openai
 import config
+
 audio_file= open("output.wav", "rb")
 openai.api_key = config.configDict["key"]
 transcript = openai.Audio.translate("whisper-1", audio_file)
-print(transcript)
+print(transcript["text"])
 
+response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt="Übersetzte auf deutsch:" + transcript["text"],
+        max_tokens=3500,
+        n=1,
+        stop=None,
+        temperature=0.1,
+        )
+
+print(response.choices[0].text)
