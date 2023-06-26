db5030d059547471d29bc774025e109d83e8627c
fifthaccess <benedikt.erkner-sach@bulme.at>
2023-06-26 08:08:10 +0200

    adding openAI voice reco to main

---
+++ test_with_AIText.py
@@ -0,0 +1,110 @@
+import openai
+import pyttsx3 as tts
+import speech_recognition as sr
+import config 
+import paho.mqtt.client  as mqtt 
+import socket
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+class AiBot: 
+    def __init__(self):
+        hostname=socket.gethostname()   
+        self.IPAddr=socket.gethostbyname(hostname)   
+
+        self._engine = tts.init()
+        
+        self._openAiKey = config.configDict["key"]
+
+        self.mqtt_client = mqtt.Client()
+        self.mqtt_client.on_connect = self.on_connect
+
+        self.mqtt_client.connect(self.IPAddr, 1883, 60) 
+
+        voices = self._engine.getProperty('voices')
+        self._engine.setProperty('voice',voices[config.configDict["voice"]].id) # 10 für dietpi; 0 für test rechner
+        
+        openai.api_key = self._openAiKey
+        openai.api_base = "https://api.openai.com/v1"
+
+    def gererateRespose(self, prompt):
+        if not("?" in prompt):
+            prompt = prompt + "?"
+        prompt = " Beantworte auf Deutsch:" + prompt 
+        print(prompt)
+            
+        response = openai.Completion.create(
+            
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=3500,
+        n=1,
+        stop=None,
+        temperature=0.1,
+        )
+        self._termianl_answer(response= response.choices[0].text)
+        self._answer(response= response.choices[0].text)
+        
+    def LED(self, prompt):
+        print(prompt)
+        if ("licht" in prompt and ("ein" in prompt or "an" in prompt)) :
+            self.mqtt_client.publish('LED', payload=1 , qos=0, retain=False)
+            self._answer("LED ein")
+            self._termianl_answer("LED ein")
+
+        if ("licht" in prompt and "aus" in prompt):
+            self.mqtt_client.publish('LED', payload=0 , qos=0, retain=False)
+            self._answer("LED aus")
+            self._termianl_answer("LED aus")
+
+
+    def _answer(self, response):
+        self._engine.say(""+ response)
+        self._engine.runAndWait()    
+    
+    def on_connect(self,client, userdata, flags, rc):
+        print(f"Connected with result code {rc}")
+
+    def _termianl_answer(self, response):
+        print(response)
+        
+    def record(self,seconds = 3 ,sampleRate = 44100):
+        print("listening")
+
+        myrecording = sd.rec(int(seconds * sampleRate), samplerate=sampleRate, channels=2)
+        sd.wait()  # Wait until recording is finished
+        write('buffer.wav', sampleRate, myrecording)  # Save as mp3 file 
+
+    def translate(self):
+
+        audio_file= open("buffer.wav", "rb")
+        openai.api_key = config.configDict["key"]
+        transcript = openai.Audio.translate("whisper-1", audio_file)
+
+        print(transcript["text"])
+
+        response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt="Übersetzte nach deutsch:" + transcript["text"],
+                max_tokens=3500,
+                n=1,
+                stop=None,
+                temperature=0.1,
+                )
+        response = response.choices[0].text
+        print(response)
+        return response
+        
+    def decide_answer(self,response):
+            if ("led" in response.lower()):
+               myAI.LED(response.lower())
+            else:
+                myAI.gererateRespose(response)
+
+
+myAI = AiBot()
+
+while True:
+   
+    myAI.record()   
+    myAI.decide_answer(myAI.translate())
